7226fbbca1bc1fd62b03548f0f3829ccc1e8b4b9
tnarasimhan <thanigaivel.narasimhan@ul.com>
2023-08-16 08:00:22 -0400

    First initialize

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducer from "./reducers";
+import {Provider} from 'react-redux';
+
+const store = createStore(allReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <Provider store= {store}>
     <App />
-  </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import counterReducer from "./counter";
+import LoggedReducer from "./isLogged.";
+
+import { combineReducers} from "redux";
+
+
+
+
+const allReducers = combineReducers({
+     counter: counterReducer,
+     isLogged : LoggedReducer
+})
+
+
+export default allReducers
+
